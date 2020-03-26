a9629438bb86b55da61bda32c974e82e7084f1da
kay-raph <kayraph3d@gmail.com>
2020-03-26 13:33:47 +0300

    added signup and login forms

--- js/script.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $("#ule").hide();
-    $("#lip").click(function () {
-        $(".link #lip").toggle(1000);
-        $(".link #ule").slideDown(1000).show(1000);
-    });
-});
