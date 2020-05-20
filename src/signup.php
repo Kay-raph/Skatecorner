603c250d677f5e0d9eae8d9189d1617a6f81ebf2
kay-raph <kayraph3d@gmail.com>
2020-05-21 02:16:03 +0300

    added styles

--- src/signup.php
+++ src/signup.php
@@ -1,28 +1,26 @@
 <?php
-// $servername = "localhost";
-// $username = "username";
-// $password = "password";
-// $dbname = "sktsignup";
+$firstName = $_POST['firstName'];
+$lastName = $_POST['lastName'];
+$DOB = $_POST['DOB'];
+$email = $_POST['email'];
+$userName = $_POST['userName'];
+$psw = $_POST['psw'];
+$gender = $_POST['gender'];
+$number = $_POST['number'];
 
-// // Create connection
-// $conn = mysqli_connect($servername, $username, $password, $dbname);
-// // Check connection
-// if (!$conn) {
-//     die("Connection failed: " . mysqli_connect_error());
-// }
-// if (mysqli_query($conn, $sql)) {
-//     echo "New Account created successfully";
-// } else {
-//     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-// }
-
-// mysqli_close($conn);
-if (isset($_POST['#pwd'])) {
-$username=$_POST['username'];
-$name=$_POST['name'];
-$email=$_POST['email'];
-$gender=$_POST['gender'];
-$password=md5($_POST['#cpwd']);
-register($loca,$name,$email,$mobile,$password,$connection);
-}
+// database connection
+  $ conn = new mysqli('localhost','root', '', 'skaters' );
+  if($conn-> connect_error){
+      die('Connection Failed :' . $conn-> connect_error);
+  }
+  else
+  {
+      $stmt =$conn->prepare("insert into signup(fisrtName, lastName, DOB, email, userName, psw, gender, number)
+      values(?,?,?,?,?,?,?,?)");
+      $stmt->bind_param("sssssssi", $firstName, $lastName, $DOB, $email, $userName, $psw, $gender, $number);
+      $stmt->execute();
+      echo "Registration done successfully"
+      $stmt->close();
+      $conn->close();
+  }
 ?>
